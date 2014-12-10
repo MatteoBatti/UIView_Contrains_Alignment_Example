8cad6a16c981feb3fd27228f678a770f28392f88
MatteoBatti <matteobattistini@Matteos-MacBook-Pro.local>
2014-12-10 21:19:33 +0100

    Initial Commit

---
+++ UIView_Contrains_Alignment_Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView_Contrains_Alignment_Example
+//
+//  Created by Matteo Battistini on 10/12/14.
+//  Copyright (c) 2014 Matteo Battistini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIView_Contrains_Alignment_Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView_Contrains_Alignment_Example
+//
+//  Created by Matteo Battistini on 10/12/14.
+//  Copyright (c) 2014 Matteo Battistini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
